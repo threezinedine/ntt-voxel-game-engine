2bf376d9bb528b063ce9cfdbdc56ebd0719b9056
threezinedine <threezinedine@gmail.com>
2025-11-28 00:30:45 +0700

    [feature] complete enum  binding

--- autogen/tests/binding/test_generate_pyi_enum.py
+++ autogen/tests/binding/test_generate_pyi_enum.py
@@ -14,7 +14,7 @@ def test_generate_simple_enum():
     result = GenerateBindings(
         binding,
         testContent="""
-enum Color {
+enum __attribute__((annotate("binding"))) Color {
     RED,
 };
 """,
@@ -38,7 +38,7 @@ def test_generate_empty_enum():
     result = GenerateBindings(
         binding,
         testContent="""
-enum Empty {
+enum __attribute__((annotate("binding"))) Empty {
 };
 """,
     )
@@ -64,7 +64,7 @@ def test_generate_enum_with_comments():
 /**
  * @brief This is an enum with comments.
  */
-enum Color {
+enum __attribute__((annotate("binding"))) Color {
     RED, ///< Red color
     GREEN,
     BLUE = 3 ///< Blue color
@@ -89,3 +89,52 @@ class Color(Enum):
 """
 
     AssertBindingResult(expect, result)
+
+
+def test_generate_enum_with_hidden_annotations():
+    binding: Binding = Binding(
+        file="",
+        template="autogen/templates/pyi/enum.j2",
+        output="",
+    )
+
+    result = GenerateBindings(
+        binding,
+        testContent="""
+enum __attribute__((annotate("binding"))) Color {
+    RED,
+    GREEN,
+    BLUE,
+    COUNT __attribute__((annotate("hidden"))),
+        """,
+    )
+
+    expect = """
+class Color(Enum):
+    RED = 0
+    GREEN = 1
+    BLUE = 2
+"""
+
+    AssertBindingResult(expect, result)
+
+
+def test_generate_enum_with_non_binding_annotation():
+    binding: Binding = Binding(
+        file="",
+        template="autogen/templates/pyi/enum.j2",
+        output="",
+    )
+
+    result = GenerateBindings(
+        binding,
+        testContent="""
+enum Color {
+    RED,
+    GREEN,
+    BLUE,
+};
+""",
+    )
+
+    AssertBindingResult("\n", result)
